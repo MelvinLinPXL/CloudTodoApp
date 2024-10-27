c91d29ea5b289d2304947303dc8e3a3105d2af67
MelvinLinPXL <116729069+MelvinLinPXL@users.noreply.github.com>
2024-10-27 23:23:06 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,22 @@ require('dotenv').config()
 
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const healthRoutes = require('./routes/health.routes');
 const todoRoutes = require('./routes/todo.routes');
 const carrouselRoutes = require('./routes/carrousel.routes');
-
-const app = express();
 const db = require('./db');
+const PORT = 8090;
 
+app.use(cors({
+  origin: '*'
+}));
 
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
 app.use(express.json());
 
 app.use('/health', healthRoutes);
-app.use('/todo', todoRoutes);
-app.use('/carrousel', carrouselRoutes)
+app.use('/todo', cors(), todoRoutes);
+app.use('/carrousel', cors(), carrouselRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
