940ec4773c2be9ea8adb13d67d35163c6e49d566
MelvinLinPXL <116729069+MelvinLinPXL@users.noreply.github.com>
2024-11-01 14:51:33 +0100

    Update carrousel.routes.js

--- dynamodb/routes/carrousel.routes.js
+++ dynamodb/routes/carrousel.routes.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const aws = require('./../aws');
+
 const carrouselRouter = express.Router();
 const carrouselData = require('./../data/carrousel.json');
 
-carrouselRouter.get('', (req, res) => {
-    res.json(carrouselData);
-});
+const s3 = new AWS.S3();
+const BUCKET_NAME = 'pxl-mel-pe2-s3-bucket';
 
+// Function to fetch images from S3 bucket
+const fetchImages = async () => {
+    const params = {
+        Bucket: BUCKET_NAME,
+    };
+
+    // List objects in the specified S3 bucket
+    const data = await s3.listObjectsV2(params).promise();
+    const images = data.Contents.map(item => item.Key); // Get the keys (file names)
+
+    // Format the URLs in the desired structure
+    return images.map(image => ({
+        url: `https://${BUCKET_NAME}.s3.amazonaws.com/${image}`
+    }));
+};
+
+carrouselRouter.get('/', async (req, res) => {
+    try {
+        const formattedImages = await fetchImages();
+        res.json(formattedImages);
+    } catch (error) {
+        console.error('Error fetching images from S3:', error);
+        res.status(500).json({ error: 'Failed to fetch images' });
+    }
+});
 
 module.exports = carrouselRouter;
