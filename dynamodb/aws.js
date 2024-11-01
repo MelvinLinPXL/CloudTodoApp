1f53d4e19e7d965dbab1eb5dd5fde8892e408c0f
MelvinLinPXL <116729069+MelvinLinPXL@users.noreply.github.com>
2024-11-01 13:35:41 +0100

    Update aws.js

--- dynamodb/aws.js
+++ dynamodb/aws.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk');
-
+/*
 AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID});
 AWS.config.update({secretAccessKey: process.env.AWS_ACCESS_KEY});
 AWS.config.update({region: process.env.AWS_REGION});
-
+*/
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports = docClient;
